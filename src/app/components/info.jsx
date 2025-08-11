4a81f53519dbc66f195f6b62d87f9af2a456ab66
snehacmyk <115646372+snehacmyk@users.noreply.github.com>
2025-08-11 17:26:49 +0530

    Add slider component and update styles
    Introduced a new Slider component using Swiper, including related CSS and SCSS files. Updated package.json to add Swiper as a dependency. Refined header, info, and index styles for improved layout and appearance. Integrated the Slider into the main page and fixed minor JSX and CSS issues.

--- src/app/components/info.jsx
+++ src/app/components/info.jsx
@@ -8,11 +8,11 @@ export default function InfoArea() {
                 <div className="row">
                     <div className="col-xl-7"></div>
                     <div className="col-xl-5">
-                        <div class={styles.contacts}>
+                        <div className={styles.contacts}>
                             <ul>
-                                <li> <a href="mailto:info@bigleap.ae"> <i class="fa fa-envelope"></i> info@bigleap.ae</a>
+                                <li> <a href="mailto:info@bigleap.ae"> <i className="fa fa-envelope"></i> info@bigleap.ae</a>
                                 </li>
-                                <li> <a href="tel:+123456789"> <i class="fa fa-phone"></i> +971 05 6646 464 </a></li>
+                                <li> <a href="tel:+123456789"> <i className="fa fa-phone"></i> +971 05 6646 464 </a></li>
                             </ul>
                         </div>
                     </div>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 // import styles from "./page.module.css";
 import styles from "./index.module.scss";
+import Slider from "./components/slider";
 
 export default function Home() {
 
@@ -8,36 +9,46 @@ export default function Home() {
 
   return (
     <>
-    
+
       {/* HERO SECTION START */}
 
       <section className={styles.hero}>
         <div className={styles.hanging}>
-          <img id="yeeha-img" src="Yeeha.png" alt="yeeha"/>
-            {/* <img id="img360" src="360.png" alt="360image" /> */}
-              <img id="character-img" src="character.png" alt="Character Image"/>
-            </div>
-          </section>
-          {/* HERO SECTION END */}
+          <img id="yeeha-img" src="Yeeha.png" alt="yeeha" />
+          {/* <img id="img360" src="360.png" alt="360image" /> */}
+          <img id="character-img" src="character.png" alt="Character Image" />
+        </div>
+      </section>
+      {/* HERO SECTION END */}
 
-          <section className={styles.about}>
-            <div className="row align-items-center">
-              <div className={styles.rowss}>
-                <div className="col-xl-4">
-                  <img src="Scate.png" alt="" className={styles.image} />
-                </div>
-                <div className="col-xl-8">
-                  <h4 className={styles.abtcnt}>
-                    Bigleap is a digital product design agency
-                    thatturns complex technology into intuitive,
-                    usable interfaces. We work with forward-thinking
-                    teams to create market-ready digital products that
-                    areeasy to use and hard to ignore.
-                  </h4>
-                </div>
-              </div>
+      <section className={styles.about}>
+        <div className="row align-items-center">
+          <div className={styles.rowss}>
+            <div className="col-xl-4">
+              <img src="Scate.png" alt="" className={styles.image} />
             </div>
-          </section>
-        </>
-        );
+            <div className="col-xl-8">
+              <h4 className={styles.abtcnt}>
+                Bigleap is a digital product design agency
+                thatturns complex technology into intuitive,
+                usable interfaces. We work with forward-thinking
+                teams to create market-ready digital products that
+                areeasy to use and hard to ignore.
+              </h4>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      {/* SLIDER SECTION */}
+
+      <section className={styles["special-head"]}>
+        <h2 className=""></h2>
+        <p className=""></p>
+
+      <Slider></Slider>
+      </section>
+    </>
+  );
 }
